4ccd3fffa3eee3b4bfced4e0c32d6df44f89081b
Chowhound <825352674@qq.com>
2024-08-10 17:00:13 +0800

    feat:基本实现Getter

--- chowh-bot-app/src/main/java/per/chowhound/bot/ChowhBotApplication.java
+++ chowh-bot-app/src/main/java/per/chowhound/bot/ChowhBotApplication.java
@@ -15,18 +15,22 @@ import java.util.List;
  */
 public class ChowhBotApplication {
     public static void main(String[] args) {
-//        Mono<Result<Bot>> bot = Getter.getBotInfo();
-//        Mono<?> mono = Getter.getGroupInfo(811545265L, true);
-//        Mono<?> mono = Getter.getGroupList();
-//        Mono<?> mono = Getter.getGroupMemberInfo(811545265L, 825352674L, true);
-//        Mono<?> mono = Getter.getGroupMemberList(811545265L);
-//        Mono<?> mono = Getter.getGroupHonorInfo(811545265L, GroupHonor.TYPE_ALL);
-//        Mono<?> mono = Getter.getCookies();
-//        Mono<?> mono = Getter.getCsrfToken();
-//        Mono<?> mono = Getter.getCredentials();
-//        Mono<?> mono = Getter.getRecord("1f0b1851392eb90a81a736fcb61b165d.silk", "mp3");
-//        Mono<?> mono = Getter.getImage("1f0b1851392eb90a81a736fcb61b165d.jpg");
-//        Mono<?> mono = Getter.getStatus();
+/*
+        Mono<?> mono = Getter.getBotInfo();
+        Mono<?> mono = Getter.getBotInfo();
+        Mono<?> mono = Getter.getMsg(97151221L);
+        Mono<?> mono = Getter.getFriendList();
+        Mono<?> mono = Getter.getGroupList();
+        Mono<?> mono = Getter.getGroupMemberInfo(811545265L, 825352674L, true);
+        Mono<?> mono = Getter.getGroupMemberList(811545265L);
+        Mono<?> mono = Getter.getGroupHonorInfo(811545265L, GroupHonor.TYPE_ALL);
+        Mono<?> mono = Getter.getCookies();
+        Mono<?> mono = Getter.getCsrfToken();
+        Mono<?> mono = Getter.getCredentials();
+        Mono<?> mono = Getter.getRecord("1f0b1851392eb90a81a736fcb61b165d.silk", "mp3");
+        Mono<?> mono = Getter.getImage("1f0b1851392eb90a81a736fcb61b165d.jpg");
+        Mono<?> mono = Getter.getStatus();
+*/
         Mono<?> mono = Getter.getVersionInfo();
 
 
